e876edde1327e447e10ab7a218661d2499e592a4
Hridesh Rajan <hridesh@cs.iastate.edu>
2014-08-08 15:01:19 -0500

    recursive environment.

--- src/reflang/Env.java
+++ src/reflang/Env.java
@@ -1,5 +1,7 @@
 package reflang;
 
+import java.util.List;
+
 /**
  * Representation of an environment, which maps variables to values.
  * 
@@ -34,6 +36,27 @@ public interface Env {
 		public Value get (String search_var) {
 			if (search_var.equals(_var))
 				return _val;
+			return _saved_env.get(search_var);
+		}
+	}
+	
+	static public class ExtendEnvRec implements Env {
+		private Env _saved_env;
+		private List<String> _names;
+		private List<Value.Fun> _funs;
+		public ExtendEnvRec(Env saved_env, List<String> names, List<Value.Fun> funs){
+			_saved_env = saved_env;
+			_names = names;
+			_funs = funs;
+		}
+		public Value get (String search_var) {
+			int size = _names.size();
+			for(int index = 0; index < size; index++) {
+				if (search_var.equals(_names.get(index))) {
+					Value.Fun f = _funs.get(index);
+					return new Value.Fun(this, f.formals(), f.body());				
+				}
+			}
 			return _saved_env.get(search_var);
 		}
 	}
